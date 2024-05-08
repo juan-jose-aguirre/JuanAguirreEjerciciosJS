794b22530639f00a4dc836ccb6e5a8854ee5b6a8
jjaguirre <juanjoa04@gmail.com>
2024-05-07 23:33:10 -0500

    Por fin termine, ahora a ponerlo bonito

--- ejerciciosDOM/notas.js
+++ ejerciciosDOM/notas.js
@@ -1,8 +1,8 @@
 let listaNotas = [
   {
     id: 1,
-    titulo: "Sacar la basura",
-    texto: "mi mama me va a retar si no lo hago",
+    titulo: "Nota1",
+    texto: "prueba1",
     realizada: false,
   },
   {
@@ -35,6 +35,7 @@ let titulo = document.getElementById("titulo");
 let descripcion = document.getElementById("descripcion");
 
 let filtroRealizada = document.getElementById("realizadas");
+let buscar = document.getElementById("buscar");
 
 btnGuardar.addEventListener("click", function () {
   agregarNota(titulo, descripcion, listaNotas);
@@ -44,6 +45,21 @@ btnLimpiar.addEventListener("click", limpiarCampos);
 filtroRealizada.addEventListener("change", function (e) {
   filtraRealizadas(listaNotas, e);
 });
+
+buscar.addEventListener("input",function(e){
+    filtraTexto(listaNotas,e.target.value);
+});
+
+function filtraTexto(arreglo,texto){
+    if (texto != "") {
+        limpiarNotas();
+        let arregloFiltrado = arreglo.filter(nota => nota.titulo.toLowerCase().includes(texto) || nota.texto.toLowerCase().includes(texto) );
+        mostrarNotas(arregloFiltrado);
+    }else{
+        limpiarNotas();
+        mostrarNotas(arreglo);
+    }
+}
 
 function filtraRealizadas(arreglo, e) {
   if (e.srcElement.checked == true) {
@@ -88,18 +104,14 @@ function agregarNota(titulo, descripcion, arreglo) {
     mostrarNotas(arreglo);
   }
 }
-function limpiarNotas() {
-  let lugar = document.getElementById("verNotas");
-  lugar.innerHTML = "";
-}
 
 function mostrarNotas(arreglo) {
   if (arreglo.length == 0) {
     let lugar = document.getElementById("verNotas");
     let aviso = document.createElement("div");
     aviso.innerHTML = `<div class="alert alert-info" role="alert">
-        No hay notas para mostrar!
-      </div>`;
+    No hay notas para mostrar!
+    </div>`;
     lugar.appendChild(aviso);
   } else {
     let lugar = document.getElementById("verNotas");
@@ -109,9 +121,9 @@ function mostrarNotas(arreglo) {
     for (let nota of arreglo) {
       let tarjeta = document.createElement("div");
       tarjeta.innerHTML = `<div class="card m-1" style="width: 13rem;">
-        <div class="card-body">
+      <div class="card-body">
         <h5 class="card-title border-bottom"><input class="me-3" onClick="marcarRealizada(${
-          nota.id
+          i
         },listaNotas)" type="checkbox" ${
         nota.realizada ? "checked" : ""
       } type="checkbox" name="realizada" id="realizada${nota.id}">${
@@ -133,8 +145,8 @@ function borrarNota(index, arreglo) {
   limpiarNotas();
   mostrarNotas(arreglo);
 }
-function marcarRealizada(id, arreglo) {
-  let nota = arreglo[id - 1];
+function marcarRealizada(index, arreglo) {
+  let nota = arreglo[index];
   nota.realizada = !nota.realizada;
 }
 
@@ -143,4 +155,8 @@ function limpiarCampos() {
   let txtTexto = document.getElementById("descripcion");
   txtTitulo.value = "";
   txtTexto.value = "";
+}
+function limpiarNotas() {
+  let lugar = document.getElementById("verNotas");
+  lugar.innerHTML = "";
 }
